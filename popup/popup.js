8b991b76366605f81643519fd7dccfbb07fb2f12
J-Henriksson <henriksson.joel2@gmail.com>
2026-04-21 03:27:04 +0200

    Clicking link to lovable closes window

--- popup/popup.js
+++ popup/popup.js
@@ -7,4 +7,5 @@ document.body.style.display = '';
 document.getElementById('open-settings').addEventListener('click', () => browser.runtime.openOptionsPage());
 document.getElementById('open-lovable').addEventListener('click', () => {
   browser.tabs.create({ url: 'https://lovable.dev' });
+  window.close();
 });
